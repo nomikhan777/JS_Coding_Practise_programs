a0b7344c584e94c60b00cd0ee41980512248be84
nomikhan777 <muhammadnomanbrw123@gmail.com>
2022-10-16 01:56:40 +0500

    arraymethod1

---
+++ ArrayMethods/script.js
@@ -0,0 +1,12 @@
+// array methods 
+let num = [1, 2, 3, 34, 44]
+console.log(num)
+
+//  tostring method
+let b = num.toString();
+console.log(b);
+
+// Another example 
+let arr = [23, 45, 555, 44, 3, 4, 3]
+let number = arr.toString();
+console.log(number)
