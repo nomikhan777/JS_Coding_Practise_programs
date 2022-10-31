9ed26a61febced9ee32f921be5a5e204a8acecde
nomikhan777 <muhammadnomanbrw123@gmail.com>
2022-10-31 14:06:20 +0500

    31thoct

---
+++ js in browser/script.js
@@ -0,0 +1,21 @@
+
+//   alert method 
+// this method takes value as a string 
+alert("your scripts works")
+
+//  confirm method is used to ask user about any aacton like delete or enter something 
+let write = confirm("do you ant to write it to the page")
+
+
+//  DOM METHOD 
+// Document object model
+//  DOM REPRESENT THE PAGE CONTENT AS A HTML 
+console.log(window)
+console.log(document)
+document.body.style.background = "red"
+
+
+
+// BOM METHOD 
+// Browser object model
+//  BOM REPRESENTS THE ADDITIONAL OBJECTS PROVIDED BY THE BROWSER FOR WORKING WITH EVERYTHING EXCEPT THE DOCUMENT
