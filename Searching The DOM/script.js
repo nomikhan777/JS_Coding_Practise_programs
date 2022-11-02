f88dcb06180c65e183adb753926d3998330aaa69
nomikhan777 <muhammadnomanbrw123@gmail.com>
2022-11-02 22:04:56 +0500

    2nd Nov

---
+++ Searching The DOM/script.js
@@ -0,0 +1,17 @@
+
+// let ctitle = document.getElementsByClassName('card-title')[0]
+
+// ctitle.style.color = "red"
+
+// let cardtitle = document.getElementById('firstcardtitle')
+
+// cardtitle.style.color = "blue"
+
+
+let Ctitles = document.querySelectorAll('.card-title');
+Ctitles[0].style.color = "blue";
+Ctitles[1].style.color = "red";
+Ctitles[2].style.color = "yellow";
+console.log(Ctitles)
+
+document.querySelector('.this').style.color = "red"
